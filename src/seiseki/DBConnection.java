f9b1ed7c66f684ba33f98b09f5b0ad9f436042a5
h_oohashi <h_oohashi@LAPTOP-6H5RGHVB>
2025-05-15 09:59:20 +0900

    student

---
+++ src/seiseki/DBConnection.java
@@ -0,0 +1,20 @@
+package seiseki;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:h2:tcp://localhost/~/seiseki";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("H2 Driver not found", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
